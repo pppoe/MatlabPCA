65ab4626ba12c20286b33d890f82f682dc530ad5
unknown <hli18@CS000032.cs.stevens-tech.edu>
2012-04-03 18:05:47 -0400

    Correct Screenshots

--- demoPCA.m
+++ demoPCA.m
@@ -1,28 +1,9 @@
 input_data = rand(1000,3);
-pack_1 = 1:300;
-pack_2 = 301:600;
-pack_3 = 601:1000;
-
-input_data(pack_1,1) = rand(size(pack_1))*0.2;
-input_data(pack_1,2) = rand(size(pack_1))*0.2;
-input_data(pack_1,3) = rand(size(pack_1));
-
-input_data(pack_2,1) = rand(size(pack_2))*0.2 + 0.3;
-input_data(pack_2,2) = rand(size(pack_2));
-input_data(pack_2,3) = rand(size(pack_2))*0.2 + 0.3;
-
-input_data(pack_3,1) = rand(size(pack_3))*0.2 + 0.6;
-input_data(pack_3,2) = rand(size(pack_3))*0.2 + 0.6;
-input_data(pack_3,3) = rand(size(pack_3));
 
 %% Visualize Input Data
 figure(1);
 hold off;
-plot3(input_data(pack_1,1), input_data(pack_1,2), input_data(pack_1,3), 'ro');
-hold on;
-plot3(input_data(pack_2,1), input_data(pack_2,2), input_data(pack_2,3), 'go');
-plot3(input_data(pack_3,1), input_data(pack_3,2), input_data(pack_3,3), 'bo');
-
+plot3(input_data(:,1), input_data(:,2), input_data(:,3), 'ro');
 
 %% Function PCA, input_data, out_dim
 % Use this to switch methods
@@ -55,9 +36,5 @@ end
 %% Visualize Output Data
 figure(2);
 hold off;
-plot(output_data(pack_1,1), output_data(pack_1,2), 'ro');
-hold on;
-plot(output_data(pack_2,1), output_data(pack_2,2), 'go');
-plot(output_data(pack_3,1), output_data(pack_3,2), 'bo');
-
+plot(output_data(:,1), output_data(:,2), 'bo');
 
